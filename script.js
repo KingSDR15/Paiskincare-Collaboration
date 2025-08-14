f1c93ba93cf0eefed78288bc2b26e34e982b210b
KingSDR15 <kingsdr15@gmail.com>
2025-08-14 23:05:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const emailIcon = `<i class="fas fa-envelope"></i>`;
       const emailLink = document.getElementById("emailLink");
       emailLink.innerHTML = `${emailIcon} Send Email`;
-      emailLink.href = `mailto:paiskincarecollaborations@gmail.com?subject=Collaboration Submission from ${name}&body=Hi,%0A%0AMy name is ${name} and I have completed the collaboration form and downloaded the agreement. Please find my attachment below.`;
+      emailLink.href = `mailto:paiskincare.creatorhub@gmail.com?subject=Collaboration Submission from ${name}&body=Hi,%0A%0AMy name is ${name} and I have completed the collaboration form and downloaded the agreement. Please find my attachment below.`;
     });
   });
 
